d86caf5ff55f577bf41552b628d468434e870943
milad <milad@miladalsh.local>
2016-10-14 00:17:13 +0200

    completing cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
+#This function creates a list contains functions for set/get the matrix and setInvMat/getInvMat 
+#to get/set inverion of the matrix
 makeCacheMatrix <- function(x = matrix()) {
+  #the variable that holds the inversion of the matrix
+  invm <- NULL
 
+  #set/get for the matrix
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  
+  #set/get for the inversion of the matrix
+  setInvMatrix <- function(m) {
+    invm <<- m
+  }
+  getInvMatrix <- function() invm
+  
+  list(set = set, get = get, setInvMat = setInvMatrix, getInvMat = getInvMatrix)
 }
 
 
-## Write a short comment describing this function
-
+#A function takes x argument which is created by makeCacheMatrix function 
+#and return the inversion of the matrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #try to get the invMat and see if its not 
+    #null then we return it
+    invMat <- x$getInvMat()
+    if(!is.null(invMat)) {
+      return(invMat)
+    }
+    
+    #if the invMat is null then calculate
+    #the inversion and cache it in the x object
+    m <- x$get()
+    invMat <- solve(m)
+    x$setInvMat(invMat)
+    invMat
 }
